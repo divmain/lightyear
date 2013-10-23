ce1c9265856c339cf41e40788ca695b3cf90789f
Dale Bustad <dale@divmain.com>
2013-10-23 01:43:00 -0700

    improved darken and lighten implementation

--- lightyear/functions/colors.py
+++ lightyear/functions/colors.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from colorsys import hls_to_rgb, rgb_to_hls
 
 from . import bifunc
 from ..types import Color, Distance
@@ -26,11 +27,12 @@ def darken(color, amount):
         return color
 
     elif isinstance(amount, Distance) and amount.unit == '%':
-        new_r = int(color._r * (100 - amount.value) / 100)
-        new_g = int(color._r * (100 - amount.value) / 100)
-        new_b = int(color._r * (100 - amount.value) / 100)
-        color._r, color._g, color._b = (color if color >= 0 else 0
-                                        for color in (new_r, new_g, new_b))
+        r, g, b = float(color._r/255), float(color._g/255), float(color._b/255)
+        h, l, s = rgb_to_hls(r, g, b)
+        new_l = l * (100 - int(amount.value)) / 100
+        color._r, color._g, color._b = (
+            Decimal(255*c) if c >= 0 else Decimal(0)
+            for c in hls_to_rgb(h, new_l, s))
         return color
 
     raise ValueError('Cannot darken by value:', str(amount))
@@ -58,11 +60,13 @@ def lighten(color, amount):
         return color
 
     elif isinstance(amount, Distance) and amount.unit == '%':
-        new_r = int(color._r * (100 + amount.value) / 100)
-        new_g = int(color._r * (100 + amount.value) / 100)
-        new_b = int(color._r * (100 + amount.value) / 100)
-        color._r, color._g, color._b = (color if color <= 255 else 255
-                                        for color in (new_r, new_g, new_b))
+        r, g, b = float(color._r/255), float(color._g/255), float(color._b/255)
+        h, l, s = rgb_to_hls(r, g, b)
+        new_l = l * (100 + int(amount.value)) / 100
+        color._r, color._g, color._b = (
+            Decimal(255*c) if c <= 255 else Decimal(255)
+            for c in hls_to_rgb(h, new_l, s))
         return color
+
 
     raise ValueError('Cannot lighten by value:', str(amount))
