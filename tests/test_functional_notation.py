3c9b470ce98a7f6571efe0d5bbae643b7097028f
Dale Bustad <dale@divmain.com>
2013-10-23 01:49:15 -0700

    better test function names

--- tests/test_functional_notation.py
+++ tests/test_functional_notation.py
@@ -26,7 +26,7 @@ def test_rgb_darken_a():
     assert ly.css() == o
 
 
-def test_hsl_darken_a():
+def test_hsl():
     i = dedent(r'''
         p
             color: hsl(190 30% 94%)
@@ -37,7 +37,7 @@ def test_hsl_darken_a():
     assert ly.css() == o
 
 
-def test_hsl_darken_b():
+def test_hsl_darken():
     i = dedent(r'''
         p
             color: darken(hsl(190 30% 94%) 10%)
@@ -46,70 +46,3 @@ def test_hsl_darken_b():
     ly = LY()
     ly.eval(i)
     assert ly.css() == o
-
-
-# def test_darken_b():
-#     i = dedent('''
-#         p
-#             color: darken(#ffffff #111111)
-#         ''')
-#     o = 'p{color:#eeeeee;}'
-#     ly = LY()
-#     ly.eval(i)
-#     assert ly.css() == o
-
-
-# def test_darken_c():
-#     i = dedent('''
-#         p
-#             color: darken(#ffffff 10)
-#         ''')
-#     o = 'p{color:#f5f5f5;}'
-#     ly = LY()
-#     ly.eval(i)
-#     assert ly.css() == o
-
-
-# def test_darken_d():
-#     i = dedent('''
-#         p
-#             color: darken(#ff1537 50)
-#         ''')
-#     o = 'p{color:#cd0005;}'
-#     ly = LY()
-#     ly.eval(i)
-#     assert ly.css() == o
-
-
-# def test_darken_e():
-#     i = dedent('''
-#         p
-#             color: darken(#ff1537 #8e114a)
-#         ''')
-#     o = 'p{color:#710400;}'
-#     ly = LY()
-#     ly.eval(i)
-#     assert ly.css() == o
-
-
-# def test_darken_f():
-#     i = dedent('''
-#         p
-#             color: darken(white 10%)
-#         ''')
-#     o = 'p{color:#e5e5e5;}'
-#     ly = LY()
-#     ly.eval(i)
-#     assert ly.css() == o
-
-
-# def test_darken_g():
-#     i = dedent('''
-#         p
-#             color: darken(white #ff9bff)
-#         ''')
-#     o = 'p{color:darkgreen;}'
-#     ly = LY()
-#     ly.eval(i)
-#     assert ly.css() == o
-
