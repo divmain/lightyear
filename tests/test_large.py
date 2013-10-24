63ba9e74b4989556e4a0ff180054c350996cadb0
Dale Bustad <dale@divmain.com>
2013-10-24 14:57:45 -0700

    larger tests

---
+++ tests/test_large.py
@@ -0,0 +1,43 @@
+from textwrap import dedent
+
+import env
+from lightyear import LY
+
+
+def test_large_a():
+    i = dedent('''
+        body
+            position: relative
+            top: 80px
+
+            > section
+                padding: 40px 16px
+                line-height: 1.4em
+
+                .main
+                    display: inline-block
+                    width: 400px
+
+                    .item
+                        display: inline-block
+                        width: 100px
+                        margin-bottom: 24px
+
+                    aside
+                        display: inline-block
+                        width: 100%
+
+                h2
+                    display: inline-block
+                    width: 100px
+        ''')
+    o = ('body{position:relative;top:80px;}'
+         'body > section{padding:40px 16px;line-height:1.4em;}'
+         'body > section .main{display:inline-block;width:400px;}'
+         'body > section .main .item{display:inline-block;width:100px;margin-bottom:24px;}'
+         'body > section .main aside{display:inline-block;width:100%;}'
+         'body > section h2{display:inline-block;width:100px;}')
+    ly = LY()
+    ly.eval(i)
+    print(ly.css())
+    assert ly.css() == o
